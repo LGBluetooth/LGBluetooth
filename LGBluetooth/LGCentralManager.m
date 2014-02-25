91d8a5ffe5d7c67713e27feb98771c9a6e6d2bb6
DavidSahakyan <loggerdev@gmail.com>
2014-02-25 19:27:57 +0400

    Copyright update

--- LGBluetooth/LGCentralManager.m
+++ LGBluetooth/LGCentralManager.m
@@ -1,6 +1,7 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2013 l0gg3r
+// Created by : l0gg3r
+// Copyright (c) 2014 SocialObjects Software. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
